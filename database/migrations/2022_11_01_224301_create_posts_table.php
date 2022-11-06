a2c6523ff20f961164196f9abe159510d235ae42
cgud3061 <cgud3061@mail4.doshisha.ac.jp>
2022-11-06 12:40:54 +0900

    マイグレーション機能とシーディングの実装

--- database/migrations/2022_11_01_224301_create_posts_table.php
+++ database/migrations/2022_11_01_224301_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->string('body', 200);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
